db555d35bf4bf1cfc63aeea80826fb2db8b09145
Inchill <173772140@qq.com>
2024-09-23 23:02:34 +0800

    chore: eslint format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,21 +5,21 @@ import pluginReact from 'eslint-plugin-react';
 
 
 export default [
-  {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      'no-unused-vars': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-      'no-console': 'warn',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      'eol-last': ['error', 'always'],
-      'indent': ['error', 4]
+    {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
+    {languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            'no-unused-vars': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'no-console': 'warn',
+            '@typescript-eslint/no-unused-expressions': 'off',
+            'eol-last': ['error', 'always'],
+            'indent': ['error', 4]
+        }
     }
-  }
 ];
